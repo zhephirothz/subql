65661b005c965e244426dd12985ae38212c9bb23
zHepHirotHz <kyojanvit@gmail.com>
2022-01-22 22:13:06 +0700

    [ADD] Module 4

---
+++ dist/mappings/mappingHandlers.d.ts
@@ -0,0 +1,5 @@
+import { SubstrateEvent } from "@subql/types";
+export declare function handleStakingReward(event: SubstrateEvent): Promise<void>;
+export declare function handleStakingRewarded(event: SubstrateEvent): Promise<void>;
+export declare function handleSumReward(event: SubstrateEvent): Promise<void>;
+export declare function handleSumRewarded(event: SubstrateEvent): Promise<void>;
